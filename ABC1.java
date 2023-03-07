0db1df8e770418b925bc0cb401ab966bd0beec80
Rockstar717 <warke.chetan710@gmail.com>
2023-03-07 19:32:46 +0530

    First commit

---
+++ ABC1.java
@@ -0,0 +1,24 @@
+package array1.java;    // two Dimensional Array.
+
+public class ABC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] []= new int[3][3];
+        a[0][0]=10;
+        a[1][0]=20;
+        a[2][0]=30;
+        a[0][1]=40;
+        a[0][2]=50;
+       
+        System.out.println(a[0][0]);
+        System.out.println(a[1][0]);
+        System.out.println(a[2][0]);
+        System.out.println(a[0][1]);
+        System.out.println(a[0][2]);
+        }
+    
+
+	}
+
+
